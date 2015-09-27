ae3ac6cfc007b02c7e8cce4d5e08761758cb709a
Walter Sagehorn <walter@Walters-MacBook-Pro.local>
2015-09-27 12:52:30 -0500

    post-hack

--- hacktx.py
+++ hacktx.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask.ext.mongoalchemy import MongoAlchemy
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
@@ -7,9 +8,10 @@ app.config['MONGOALCHEMY_DATABASE'] = 'hacktx'
 db = MongoAlchemy(app)
 
 class Course(db.Document):
+    deptAb = db.StringField()
     deptName = db.StringField()
     courseNum = db.StringField()
-    courseNum = db.StringField()
+    courseName = db.StringField()
     teacherName = db.StringField()
 
 class StudyInstance(db.Document):
@@ -19,7 +21,22 @@ class StudyInstance(db.Document):
 	notes = db.StringField()
 
 
+@app.route('/search/dept/<ab>')
+def search_dept(ab):
+ 	c = Course.query.filter(Course.deptAb == ab).all()
+ 	l = []
+ 	for course in c:
+ 		l.append([course.courseNum, course.courseName])
+	return jsonify(l)
+
+@app.route('/search/course/<num>')
+def search_course(num):
+ 	c = Course.query.filter(Course.courseNum == num).all()
+ 	l = []
+ 	for course in c:
+ 		l.append([course.teacherName, ObjectId(course)])
+	return jsonify(l)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
--- scraper.py
+++ scraper.py
@@ -5,8 +5,12 @@ from bs4 import BeautifulSoup
 
 meant to populate databases
 
-"""
 
+
+DISCLAIMER: This code was written under a tight time constraint, it (mostly) does not reflect my "neat" code. 
+
+"""
+base_url = "https://www.myedu.com"
 
 def main():
 	url = "https://www.myedu.com/University-of-Texas-at-Austin/school/2719/course/by-department/"
@@ -18,17 +22,75 @@ def main():
 	cnam = "";
 	teach = "";
 
-	soup = BeautifulSoup(r.text, 'html.parser').find("tbody")
-	for link in soup.find_all("tr"):
-		for linkd in soup.find_all("td"):
-			for linkd in soup.find_all("a"):
-				print linkd.get("class")
+	soup = BeautifulSoup(r.text, 'html.parser')
+	soup = soup.table
+	#print soup.prettif
+
+	#find_teacher("https://www.myedu.com/University-of-Texas-at-Austin/E-316L-British-Literature/course/8396013/", "er","er","er","er")
+
+	
+	for i in range(len(soup.find_all("td"))):
+		try:
+			#print i
+			if soup.find_all("td")[i].a["class"] == ["abbreviation"]:
+				ab = soup.find_all("td")[i].a.text.strip()
+			if soup.find_all("td")[i].a["class"] == ["name"]:
+				nm = soup.find_all("td")[i].a.text.strip()
+				find_course(soup.find_all("td")[i].a["href"], ab, nm)
+		except:
+			#print "cant"
+			pass
 
 
+	"""
+	for i in range(len(soup.find_all("td"))):
+		try:
+			print soup.find_all("td")[i].a["class"]
+		except:
+			pass"""
 
 
+def find_course(url, ab, nm):
+	print "course " + url + "    " + ab + "   " + nm
 
+	r = requests.get(base_url + url)
+	soup = BeautifulSoup(r.text, 'html.parser')
+	soup = soup.table
+	#print soup.prettify()
 
+	for i in range(len(soup.find_all("td"))):
+		try:
+			#print str(i) + "uuuu"
+			if soup.find_all("td")[i].a["class"] == ["abbreviation"]:
+				cnum = soup.find_all("td")[i].a.text.strip()
+			if soup.find_all("td")[i].a["class"] == ["name"]:
+				cnam = soup.find_all("td")[i].a.text.strip()
+				find_teacher(soup.find_all("td")[i].a["href"], ab, nm, cnum, cnam)
+		except:
+			#print "cant teach"
+			pass
+
+def find_teacher(url, ab, nm, cnum, cnam):
+	print "teach " + url + "    " + ab + "   " + nm + " " + cnum + " " + cnam
+
+	r = requests.get(base_url + url)
+	soup = BeautifulSoup(r.text, 'html.parser')
+	soup = soup.table
+
+	for i in soup.find_all("td"):
+		try:
+			if i["class"] == ["name"]:
+				tea = i.a.text.strip()
+				make_course(ab, nm, cnum, cnam, tea)
+		except:
+			#print "cant"
+			pass
+
+def make_course(ab, nm, cnum, cnam, tea):
+	print ab + " " + nm + " " + cnum + " " + cnam + " " + tea
+	myCourse = Course(deptAb=ab, deptName=nm, courseNum=cnum, courseName=cnam, teacherName=tea)
+	myCourse.save()
+	
 
 
 if __name__ == "__main__":
